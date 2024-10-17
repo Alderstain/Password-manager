e58f8826546e070474f24b6fd414bf8a6a64cb7d
Alderstain <bakhmin@bk.ru>
2024-10-18 01:36:31 +0300

    Second commit

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"math/rand/v2"
-	"testing"
 )
 
 type account struct {
@@ -12,37 +11,33 @@ type account struct {
 	url      string
 }
 
-func main() {
-	dandan := generatePassword(6)
-	account := account{
-		login:    "bakhmnmn",
-		password: dandan,
-		url:      "google.com",
+func (acc *account) generatePassword(n int) {
+	password := make([]rune, n)
+	for i := 0; i < n; i++ {
+		password[i] = rand.Int32N(73) + 50 // от 50 символа до 123 включительно
 	}
-	printMyData(account)
+	acc.password = string(password)
+}
+
+func main() {
+	newLogin := getData("Введите логин: ")
+	newUrl := getData("Введите url: ")
+	myAccount := account{
+		login: newLogin,
+		url:   newUrl,
+	}
+	myAccount.generatePassword(12)
+	printMyData(&myAccount)
 
 }
 
-func printMyData(acc account) {
+func printMyData(acc *account) {
 	fmt.Print(acc.login, " ", acc.password, " ", acc.url)
 }
 
-func generatePassword(n int) string {
-	password := make([]rune, n)
-	for i := 0; i < n; i++ {
-		password[i] = rand.Int32N(73) + 50
-	}
-	return string(password)
+func getData(message string) string {
+	fmt.Print(message)
+	var data string
+	fmt.Scanln(&data)
+	return data
 }
-
-func testGeneratePassword(t *testing.T) {
-	a := 6
-	result := generatePassword(a)
-	expected := len(result)
-
-	if expected != 6 {
-		t.Errorf("generatePassword(6): expected string of len = %d", a)
-	}
-}
-
-// от 50 символа до 123 включительно
